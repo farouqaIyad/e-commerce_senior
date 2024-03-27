23e17057a25ff385c317358e65549e126888c260
odai sa <farok1908@outlook.com>
2024-03-28 02:01:59 +0300

    'updated'

--- Users/urls.py
+++ Users/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
-from .views import signup,deleteUser,show_users,CustomerView
+from .views import AdminList,AdminDetail,CustomerList,CustomerDetail,SupplierList,SupplierDetail\
+    ,CustomerSupporterList,CustomerSupporterDetail,DriverList,DriverDetail
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 urlpatterns = [
-    path('signup/', signup, name='signup'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('delete/',deleteUser),
-    path('view/<str:pk>',CustomerView.as_view()),
-    
-
-
+    path('admin/',AdminList.as_view()),
+    path('admin/<str:pk>',AdminDetail.as_view()),
+    path('customer/',CustomerList.as_view()),
+    path('customer/<str:pk>',CustomerDetail.as_view()),
+    path('supplier/',SupplierList.as_view()),
+    path('supplier/<str:pk>',SupplierDetail.as_view()),
+    path('driver',DriverList.as_view()),
+    path('driver/<str:pk>',DriverDetail.as_view()),
+    path('customersupporter/',CustomerSupporterList.as_view()),
+    path('customersupporter/<str:pk>',CustomerSupporterDetail.as_view()),
 ]
--- Users/views.py
+++ Users/views.py
@@ -5,27 +5,163 @@ from rest_framework.permissions import AllowAny,IsAuthenticated
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.db import transaction
-from .serializers import CustomerSerializer
-from .models import Customer
-from rest_framework import mixins
-from rest_framework import generics
+from .serializers import CustomerSerializer,AdminSerializer,CustomerSupporterSerializer,DriverSerializer,SupplierSerializer
+from .models import Admin,CustomerSupporter,Driver,Supplier,Customer
+from rest_framework.views import APIView
 
-class CustomerView(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+
+class CustomerList(APIView):
+    
+    def post(self, request, format=None):
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomerDetail(APIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+    def get_object(self, pk):
+        try:
+            return Customer.objects.get(pk=pk)
+        except Customer.DoesNotExist:
+            raise Http404
 
-    def post(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
+    def get(self, request, pk, format=None):
+        customer = self.get_object(pk)
+        serializer = CustomerSerializer(customer)
+        return Response(serializer.data)
 
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+    def delete(self, request, pk, format=None):
+        customer = self.get_object(pk)
+        #here need to make sure that who is deleting is an admin
+        customer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
+class AdminList(APIView):
+    
+    def post(self, request, format=None):
+        serializer = AdminSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AdminDetail(APIView):
+    queryset = Admin.objects.all()
+    serializer_class = AdminSerializer
+
+    def get_object(self, pk):
+        try:
+            return Admin.objects.get(pk=pk)
+        except Admin.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        admin = self.get_object(pk)
+        serializer = AdminSerializer(admin)
+        return Response(serializer.data)
+
+    def delete(self, request, pk, format=None):
+        admin = self.get_object(pk)
+        #here need to update condition in case admin is content manager
+        admin.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class SupplierList(APIView):
+    
+    def post(self, request, format=None):
+        serializer = Serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SupplierDetail(APIView):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+
+    def get_object(self, pk):
+        try:
+            return Supplier.objects.get(pk=pk)
+        except Supplier.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        supplier = self.get_object(pk)
+        serializer = SupplierSerializer(supplier)
+        return Response(serializer.data)
+
+    def delete(self, request, pk, format=None):
+        supplier = self.get_object(pk)
+        #here need to update
+        supplier.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class DriverList(APIView):
+    
+    def post(self, request, format=None):
+        serializer = Serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DriverDetail(APIView):
+    queryset = Driver.objects.all()
+    serializer_class = DriverSerializer
+
+    def get_object(self, pk):
+        try:
+            return Driver.objects.get(pk=pk)
+        except Driver.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        driver = self.get_object(pk)
+        serializer = DriverSerializer(driver)
+        return Response(serializer.data)
+
+    def delete(self, request, pk, format=None):
+        driver = self.get_object(pk)
+        #here need to update
+        driver.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CustomerSupporterList(APIView):
+    
+    def post(self, request, format=None):
+        serializer = Serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CustomerSupporterDetail(APIView):
+    queryset = CustomerSupporter.objects.all()
+    serializer_class = CustomerSupporterSerializer
+
+    def get_object(self, pk):
+        try:
+            return CustomerSupporter.objects.get(pk=pk)
+        except CustomerSupporter.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        customer_supporter = self.get_object(pk)
+        serializer = CustomerSupporterSerializer(customer_supporter)
+        return Response(serializer.data)
+
+    def delete(self, request, pk, format=None):
+        customer_supporter = self.get_object(pk)
+        #here need to update
+        customer_supporter.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET'])
 def view_something(request):
